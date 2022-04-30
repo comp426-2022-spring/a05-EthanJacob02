0534cd241c4c965045decf376fb6bedfb067098c
EthanJacob02 <ethanj02@email.unc.edu>
2022-04-30 01:16:55 -0400

    main.js update

--- public/main.js
+++ public/main.js
@@ -1,19 +1,11 @@
-const coin = document.getElementById("coin")
-
+const coin = document.getElementById("flipOne")
 coin.addEventListener("click", flipCoin)
-
 async function flipCoin() {
-
     const endpoint = "app/flip/"
-
     const url = document.baseURI+endpoint
-
-    await fetch(url)
-
-  		    .then(function(response) {
+    await fetch(url).then(function(response) {
     		    return response.json();
-  		      })
-			    .then(function(result) {
+  		      }).then(function(result) {
 				    console.log(result);
 				    document.getElementById("result").innerHTML = result.flip;
 				    document.getElementById("quarter").setAttribute("src", "assets/img/"+result.flip+".png");
